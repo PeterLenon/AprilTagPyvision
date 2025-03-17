ccfbdfe1b39cca64dd90161cd94f51071f906289
PeterLenon <goshomip@gmail.com>
2025-03-17 13:59:27 -0500

    refactor

--- pyvision/videoProcessing.py
+++ pyvision/videoProcessing.py
@@ -12,20 +12,27 @@ def video_processing(frame_queue):
 		if not frame_queue.empty():
 			frame = frame_queue.get()
 
-			payloads = tag_detection(frame=frame, detector=apriltagDetector)
+			payloads = _tag_detection(frame=frame, detector=apriltagDetector)
 			for info in payloads:
 				logger.info(info)
 			
 			buckets_and_stones = getPos(frame=frame)
-			for bucket in buckets_and_stones["buckets"]:
-				x, y = bucket
-				logger.info(f"Bucket x_depth --> {x} mm, Bucket y_depth --> {y} mm")
+			if "buckets" in buckets_and_stones.keys():
+				for bucket in buckets_and_stones["buckets"]:
+					x, y = bucket
+					logger.info(f"Bucket x_depth --> {x} mm, Bucket y_depth --> {y} mm")
+			else:
+				logger.info("No buckets detected!")
 			
-			for stone in buckets_and_stones["stones"]:
-				x, y = stone
-				logger.info(f"Stone x_depth --> {x} mm, Stone y_depth --> {y} mm")
+			if "stones" in buckets_and_stones.keys():
+				for stone in buckets_and_stones["stones"]:
+					x, y = stone
+					logger.info(f"Stone x_depth --> {x} mm, Stone y_depth --> {y} mm")
+			else:
+				logger.info("No stones detected!")
+				
 
-def tag_detection(frame, detector):
+def _tag_detection(frame, detector):
 	detector = apriltag("tag36h11")
 	gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
 	detections = detector.detect(gray)
