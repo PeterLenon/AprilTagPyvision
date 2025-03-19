385373acf8f1476018bfbd4ed6839b4502ef60bd
PeterLenon <goshomip@gmail.com>
2025-03-19 16:25:54 -0500

    added frames positional argument to allow use to control the duration of the detection process

--- pyvision/main.py
+++ pyvision/main.py
@@ -1,20 +1,20 @@
 import cv2
-import multiprocessing
 import numpy as np
 from apriltag import apriltag
 from picamera2 import Picamera2
-import subprocess
 from videoStreaming import video_streaming 
 from videoProcessing import video_processing
 from queue import Queue
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--sinkfile', help='Set filepath of the sinkfile jsonfile with the stone locations',required=True)
+parser.add_argument('--sinkfile', required=True, help='Set filepath of the sinkfile jsonfile with the stone locations')
+parser.add_argument('--frames', required=False, help="set the amount of frames the camera takes for processing. The lower the number the faster the process completes")
 args = parser.parse_args()
 sinkfile = args.sinkfile
+frames = args.frames
 
 if __name__ == "__main__":
-	frame_queue = Queue(maxsize=50)
+	frame_queue = Queue(maxsize=frames if frames else 30)
 	frame_queue = video_streaming(frame_queue=frame_queue)
 	video_processing(frame_queue=frame_queue, sinkfile=sinkfile)
