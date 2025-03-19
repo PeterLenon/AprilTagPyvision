460ff627f5de992c005ae2c63faeebf65f485eaa
PeterLenon <goshomip@gmail.com>
2025-03-19 18:58:23 -0500

    refactor

--- pyvision/videoProcessing.py
+++ pyvision/videoProcessing.py
@@ -7,20 +7,20 @@ from tagDetection import tag_detection
 
 def video_processing(frame_queue, sinkfile):
 	stones_and_apriltags = dict()
-	_writeToFile(filepath=sinkfile, content="")
+	_writeToFile(filepath=sinkfile)
 
 	while not frame_queue.empty():
 		frame = frame_queue.get()
 		payloads = tag_detection(frame=frame)
 		if payloads:
-			stones_and_apriltags['stones'] = set()
+			stones_and_apriltags["stones"] = set()
 			for info in payloads:
 				logger.info(info)
 				stones_and_apriltags["apriltags"].add(info)
 
 		stones = getPos(frame=frame)
 		if "stones" in stones.keys():
-			stones_and_apriltags['apriltags'] = set()
+			stones_and_apriltags["apriltags"] = set()
 			for stone in stones["stones"]:
 				x, y = stone
 				logger.info(f"Stone x_angle --> {x} degrees, Stone y_depth --> {y} mm")
@@ -29,11 +29,11 @@ def video_processing(frame_queue, sinkfile):
 			logger.info("No stones detected!")
 
 	if stones_and_apriltags.keys():
-		stones_and_apriltags["apriltags"] = list(stones_and_apriltags['apriltags'])
+		stones_and_apriltags["apriltags"] = list(stones_and_apriltags["apriltags"])
 		stones_and_apriltags["stones"] = list(stones_and_apriltags["stones"])
 		_writeToFile(filepath=sinkfile, content=repr(stones_and_apriltags))
 
-def _writeToFile(filepath="", content="{'stones' : [], 'apriltags': []}"):
+def _writeToFile(filepath, content='{"stones" : [], "apriltags": []}'):
 	with open(file=filepath, mode="w") as file:
 		file.write(content)
 		file.close()
