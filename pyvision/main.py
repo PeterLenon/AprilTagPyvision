c314e92a1acdb4f40ce10bdbe8ef94162fbcbb1a
PeterLenon <goshomip@gmail.com>
2025-03-19 14:40:49 -0500

    remove libcamera-hello

--- pyvision/main.py
+++ pyvision/main.py
@@ -10,7 +10,7 @@ from queue import Queue
 import sys
 
 if __name__ == "__main__":
-	result = subprocess.run(["libcamera-hello"])
+	# result = subprocess.run(["libcamera-hello"])
 	frame_queue = Queue(maxsize=600)
 	frame_queue = video_streaming(frame_queue=frame_queue)
 	video_processing(frame_queue=frame_queue)
