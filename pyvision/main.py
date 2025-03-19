b0df47d479e07e5563075efb70cee068acb2d7f0
PeterLenon <goshomip@gmail.com>
2025-03-19 15:05:56 -0500

    refactor

--- pyvision/main.py
+++ pyvision/main.py
@@ -8,12 +8,17 @@ from videoStreaming import video_streaming
 from videoProcessing import video_processing
 from queue import Queue
 import sys
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--sinkfile', help='Set filepath of the sinkfile jsonfile with the stone locations',required=True)
+args = parser.parse_args()
+sinkfile = args.sinkfile
 
 if __name__ == "__main__":
-	# result = subprocess.run(["libcamera-hello"])
 	frame_queue = Queue(maxsize=600)
 	frame_queue = video_streaming(frame_queue=frame_queue)
-	video_processing(frame_queue=frame_queue)
+	video_processing(frame_queue=frame_queue, sinkfile=sinkfile)
 		
 
 
--- pyvision/videoProcessing.py
+++ pyvision/videoProcessing.py
@@ -1,12 +1,12 @@
-import cv2
 from loguru import logger
 import numpy as np
 from apriltag import apriltag
 from picamera2 import Picamera2
 from stoneDetection import getPos
 from tagDetection import tag_detection
+import json
 
-def video_processing(frame_queue):
+def video_processing(frame_queue, sinkfile):
 	logged_stones_false = False
 	while not frame_queue.empty():
 		frame = frame_queue.get()
@@ -16,11 +16,18 @@ def video_processing(frame_queue):
 
 		stones = getPos(frame=frame)
 		if "stones" in stones.keys():
+			_writeToFile(sinkfile, json.dumps(stones))
 			for stone in stones["stones"]:
 				x, y = stone
 				logger.info(f"Stone x_angle --> {x} degress, Stone y_depth --> {y} mm")
 				logged_stones_false = False
+			
 		else:
 			if not logged_stones_false:
 				logger.info("No stones detected!")
 				logged_stones_false = True
+
+def _writeToFile(filepath, content):
+	with open(file=filepath, mode="w") as file:
+		file.write(content)
+		file.close()
